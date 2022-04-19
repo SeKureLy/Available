c41138e041ad4c44e7bb172333f4ed630318d44e
BryanHY <53143180+BryanHY@users.noreply.github.com>
2022-04-19 15:15:32 +0800

    Create event table

---
+++ app/db/migrations/001_events_create.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:events) do
+      primary_key :uuid
+      foreign_key :calendar_id, table: :calendars
+
+      Integer :uuid
+      Integer :calendar_id
+      String :title
+      DateTime :start_time
+      DateTime :end_time
+      String :description
+      Integer :created_by
+      Integer :share_id
+    end
+  end
+end
