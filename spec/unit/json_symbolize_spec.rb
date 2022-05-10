63689cd30d950a40fe022bc929daa33217289e26
RayhungKao <ryamkao2339@gmail.com>
2022-05-10 14:57:10 +0800

    update

---
+++ spec/unit/json_symbolize_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../spec_helper'
+
+describe 'Test JsonRequestBody symbolizing' do
+  it 'HAPPY: should symbolize jsonified hashes' do
+    correct_hash = { username: 'testuser', password: 'testpass' }
+    json = correct_hash.to_json
+    _(JSON.parse(json)).wont_equal correct_hash
+    _(JsonRequestBody.parse_symbolize(json)).must_equal correct_hash
+  end
+end
