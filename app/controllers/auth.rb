d1f52b4b36a3a72b6e29162e68209c46ddc21f6a
BryanHY <53143180+BryanHY@users.noreply.github.com>
2022-05-17 15:04:07 +0800

    Added "Issue auth tokens after authentication"

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,6 +7,24 @@ module Available
   # Web controller for Available API
   class Api < Roda
     route('auth') do |routing|
+      routing.on 'register' do
+        # POST api/v1/auth/register
+        routing.post do
+          reg_data = JsonRequestBody.parse_symbolize(request.body.read)
+          VerifyRegistration.new(reg_data).call
+
+          response.status = 202
+          { message: 'Verification email sent' }.to_json
+        rescue VerifyRegistration::InvalidRegistration => e
+          routing.halt 400, { message: e.message }.to_json
+        rescue VerifyRegistration::EmailProviderError
+          routing.halt 500, { message: 'Error sending email' }.to_json
+        rescue StandardError => e
+          Api.logger.error "Could not verify registration: #{e.inspect}"
+          routing.halt 500
+        end
+      end
+      
       routing.is 'authenticate' do
         # POST /api/v1/auth/authenticate
         routing.post do
