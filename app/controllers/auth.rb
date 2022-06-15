dba86d8e7232bb564605882700a476fc77966f95
RayhungKao <ryamkao2339@gmail.com>
2022-06-15 14:05:24 +0800

    update routes for signed request

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,11 +7,18 @@ module Available
   # Web controller for Available API
   class Api < Roda
     route('auth') do |routing|
+      # All requests in this route require signed requests
+      begin
+        params = request.body.read
+        @request_data = SignedRequest.new(Api.config).parse(params)
+      rescue SignedRequest::VerificationError
+        routing.halt '403', { message: 'Must sign request' }.to_json
+      end
+
       routing.on 'register' do
         # POST api/v1/auth/register
         routing.post do
-          reg_data = JsonRequestBody.parse_symbolize(request.body.read)
-          VerifyRegistration.new(reg_data).call
+          VerifyRegistration.new(@request_data).call
 
           response.status = 202
           { message: 'Verification email sent' }.to_json
@@ -28,8 +35,7 @@ module Available
       routing.is 'authenticate' do
         # POST /api/v1/auth/authenticate
         routing.post do
-          credentials = JsonRequestBody.parse_symbolize(request.body.read)
-          auth_account = AuthenticateAccount.call(credentials)
+          auth_account = AuthenticateAccount.call(@request_data)
           auth_account.to_json
         rescue AuthenticateAccount::UnauthorizedError
           routing.halt '403', { message: 'Invalid credentials' }.to_json
@@ -37,10 +43,8 @@ module Available
       end
 
       # POST /api/v1/auth/sso
-      routing.post 'sso' do
-        auth_request = JsonRequestBody.parse_symbolize(request.body.read)
-        
-        auth_account = AuthorizeSso.new.call(auth_request[:access_token])
+      routing.post 'sso' do        
+        auth_account = AuthorizeSso.new.call(@request_data[:access_token])
         { data: auth_account }.to_json
       rescue StandardError => error
         puts "FAILED to validate Google account: #{error.inspect}"
