6b7167eed3c76a8179e61833328bd266d2f71172
BryanHY <53143180+BryanHY@users.noreply.github.com>
2022-06-18 23:09:50 +0800

    Updated event creation's error report

--- app/services/create_event_for_calendar.rb
+++ app/services/create_event_for_calendar.rb
@@ -8,12 +8,23 @@ module Available
         'You are not allowed to add more events'
       end
     end
+
+    # Error for requests with illegal attributes
+    class IllegalRequestError < StandardError
+      def message
+        'Cannot create an event with those attributes'
+      end
+    end
+
     def self.call(auth:, cal_id:, event_data:)
       calendar = Calendar.first(id: cal_id)
+
       policy = CalendarPolicy.new(auth[:account], calendar, auth[:scope])
       raise ForbiddenError unless policy.can_add_events?
-      Calendar.first(id: cal_id)
-              .add_event(event_data)
+
+      Calendar.first(id: cal_id).add_event(event_data)
+    rescue Sequel::MassAssignmentRestriction
+      raise IllegalRequestError
     end
   end
 end
