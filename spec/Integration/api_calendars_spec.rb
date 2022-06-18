822a5766d601aece941c1430ed136e528f80e8d3
BryanHY <53143180+BryanHY@users.noreply.github.com>
2022-06-19 01:11:33 +0800

    Updated tests for calendar

--- spec/Integration/api_calendars_spec.rb
+++ spec/Integration/api_calendars_spec.rb
@@ -3,20 +3,18 @@
 require_relative '../spec_helper'
 
 describe 'Test Calendar Handling' do
-  # include Rack::Test::Methods
+  include Rack::Test::Methods
 
   before do
     wipe_database
-    @account_data = DATA[:accounts][0]
-    @account = Available::Account.create(@account_data)
 
-    @req_header = { 'CONTENT_TYPE' => 'application/json' }
-    account_data = DATA[:accounts][0]
-    # credentials = { username: account_data['username'],
-    #                 password: account_data['password'] }
-    # post 'api/v1/auth/authenticate', credentials.to_json, @req_header
-    # @auth_token = JSON.parse(last_response.body)['attributes']['auth_token']
-    @auth_header = auth_header(account_data)
+    @account_data = DATA[:accounts][0]
+    @wrong_account_data = DATA[:accounts][1]
+
+    @account = Available::Account.create(@account_data)
+    @wrong_account = Available::Account.create(@wrong_account_data)
+
+    header 'CONTENT_TYPE', 'application/json'
   end
 
   describe 'Getting calendars' do
@@ -27,17 +25,20 @@ describe 'Test Calendar Handling' do
       end
 
       it 'HAPPY: should get list for authorized account' do
-        auth = Available::AuthenticateAccount.call(
-          username: @account_data['username'],
-          password: @account_data['password']
-        )
-
-        header 'AUTHORIZATION', "Bearer #{auth[:attributes][:auth_token]}"
+        header 'AUTHORIZATION', auth_header(@account_data)
         get 'api/v1/calendars'
-
         _(last_response.status).must_equal 200
+
         result = JSON.parse last_response.body
         _(result['data'].count).must_equal 2
+      end
+
+      it 'BAD: should not process without authorization' do
+        get 'api/v1/calendars'
+        _(last_response.status).must_equal 403
+
+        result = JSON.parse last_response.body
+        _(result['data']).must_be_nil
       end
 
       it 'BAD: should not process for unauthorized account' do
@@ -51,36 +52,43 @@ describe 'Test Calendar Handling' do
     end
 
     it 'HAPPY: should be able to get details of a single calendar' do
-      existing_calendar = DATA[:calendars][1]
-      @account.add_owned_calendar(DATA[:calendars][1])
-      id = Available::Calendar.first.id
+      calendar = @account.add_owned_calendar(DATA[:calendars][0])
 
-      header 'AUTHORIZATION', @auth_header
-      get "/api/v1/calendars/#{id}"
+      header 'AUTHORIZATION', auth_header(@account_data)
+      get "/api/v1/calendars/#{calendar.id}"
       _(last_response.status).must_equal 200
 
-      result = JSON.parse last_response.body
-      result = result['data']
-      _(result['data']['attributes']['id']).must_equal id
-      _(result['data']['attributes']['title']).must_equal existing_calendar['title']
-      _(result['data']['attributes']['created_by']).must_equal existing_calendar['created_by']
-      _(result['data']['attributes']['share_id']).must_equal existing_calendar['share_id']
+      result = JSON.parse(last_response.body)['data']['data']
+      _(result['attributes']['id']).must_equal calendar.id
+      _(result['attributes']['title']).must_equal calendar.title
     end
 
     it 'SAD: should return error if unknown calendar requested' do
-      header 'AUTHORIZATION', @auth_header
+      header 'AUTHORIZATION', auth_header(@account_data)
       get '/api/v1/calendars/foobar'
 
       _(last_response.status).must_equal 404
     end
 
-    it 'SECURITY: should prevent basic SQL injection targeting IDs' do
-      Available::Calendar.create(title: 'New Calendar')
-      Available::Calendar.create(title: 'Newer Calendar')
-      header 'AUTHORIZATION', @auth_header
+    it 'BAD AUTHORIZATION: should not get calendar with wrong authorization' do
+      calendar = @account.add_owned_calendar(DATA[:calendars][0])
+
+      header 'AUTHORIZATION', auth_header(@wrong_account_data)
+      get "/api/v1/calendars/#{calendar.id}"
+      _(last_response.status).must_equal 403
+
+      result = JSON.parse last_response.body
+      _(result['attributes']).must_be_nil
+    end
+
+    it 'BAD SQL VULNERABILTY: should prevent basic SQL injection of id' do
+      @account.add_owned_calendar(DATA[:calendars][0])
+      @account.add_owned_calendar(DATA[:calendars][1])
+
+      header 'AUTHORIZATION', auth_header(@account_data)
       get 'api/v1/calendars/2%20or%20id%3E0'
 
-      # deliberately not reporting error -- don't give attacker information
+      # deliberately not reporting detection -- don't give attacker information
       _(last_response.status).must_equal 404
       _(last_response.body['data']).must_be_nil
     end
@@ -88,13 +96,13 @@ describe 'Test Calendar Handling' do
 
   describe 'Creating New Calendars' do
     before do
-      @req_header = { 'CONTENT_TYPE' => 'application/json' }
-      @calendar_data = DATA[:calendars][1]
+      @calendar_data = DATA[:calendars][0]
     end
 
     it 'HAPPY: should be able to create new calendars' do
-      header 'AUTHORIZATION', @auth_header
+      header 'AUTHORIZATION', auth_header(@account_data)
       post 'api/v1/calendars', @calendar_data.to_json
+
       _(last_response.status).must_equal 201
       _(last_response.header['Location'].size).must_be :>, 0
 
@@ -103,16 +111,24 @@ describe 'Test Calendar Handling' do
 
       _(created['id']).must_equal calendar.id
       _(created['title']).must_equal @calendar_data['title']
-      _(created['created_by']).must_equal @calendar_data['created_by']
-      _(created['share_id']).must_equal @calendar_data['share_id']
+    end
+
+    it 'SAD: should not create new calendar without authorization' do
+      post 'api/v1/calendars', @calendar_data.to_json
+
+      created = JSON.parse(last_response.body)['data']
+
+      _(last_response.status).must_equal 403
+      _(last_response.header['Location']).must_be_nil
+      _(created).must_be_nil
     end
 
     it 'SECURITY: should not create calendar with mass assignment' do
       bad_data = @calendar_data.clone
       bad_data['created_at'] = '1900-01-01'
 
-      header 'AUTHORIZATION', @auth_header
-      post 'api/v1/calendars', bad_data.to_json, @req_header
+      header 'AUTHORIZATION', auth_header(@account_data)
+      post 'api/v1/calendars', bad_data.to_json
 
       _(last_response.status).must_equal 400
       _(last_response.header['Location']).must_be_nil
